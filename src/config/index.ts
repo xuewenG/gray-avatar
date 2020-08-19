4ff668fc6de5378055d701cf6567f7b7066047c1
xuewenG <gexuewen@outlook.com>
2020-08-19 22:22:56 +0800

    fix(avatar): change-default-context-path

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,6 @@ const env = process.env
 
 export const serverConfig = {
   port: env.PORT || 8082,
-  contextPath: env.CONTEXT_PATH || '',
+  contextPath: env.CONTEXT_PATH || '/gray-avatar',
   corsOrigin: (env.CORS_ORIGIN || '').split(',')
 }
